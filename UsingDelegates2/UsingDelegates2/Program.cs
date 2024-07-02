324fffcb6d924a63aeaeb16878d032a710dcbdd3
Nqobza25 <132018223+Nqobza25@users.noreply.github.com>
2024-07-02 18:01:53 +0200

    Add files via upload

---
+++ UsingDelegates2/UsingDelegates2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingDelegates2
+{
+    internal class Program
+    {
+        //delegate
+        delegate void PrintDelegate(string msg);
+
+
+
+        static void Main(string[] args)
+        {
+
+            ConnectToDB(PrintToCosole);
+            ConnectToDB(PrintToFile);
+
+
+        }
+        static void PrintToCosole(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void PrintToFile(string message)
+        {
+            //logic to print to file 
+        }
+        static void ConnectToDB(PrintDelegate logFile)
+        {
+
+            //LOGIC TO CONNNECT TO DATABASES
+            logFile("Connected to DB");
+        }
+
+    }
+}
---
+++ usingDeleagate/usingDeleagate/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingDeleagate
+{
+    internal class Program
+    {
+        delegate bool  FillitrDelegate(int x);
+        static void Main(string[] args)
+        {
+
+           
+
+            List<int> number=new List<int>{ 3,76,35,-13,-34,44,66};
+            FillitrDelegate even = IsEven;
+            Disply(number, even ,"Even");
+            Disply(number, IsOdd,"Odd");
+            Disply(number, IsEven,"Even");
+            Disply(number, isPositive,"Positive" );
+            Disply(number, isMulipleOfThree, "Multiple of three");
+            Console.WriteLine("++++++++++++++++++++++++");
+        
+        }
+        static bool isPositive(int x)
+        { return x >= 0; }
+        static bool IsEven(int x)
+        {
+             
+            
+            
+                return x % 2 == 0;
+
+
+
+
+
+        }
+        static bool IsOdd(int x)
+        {
+
+
+
+            return x % 2 != 0;
+
+
+
+
+
+        }
+        static bool isMulipleOfThree(int x)
+        {
+
+            return x % 3 == 0;
+        }
+        static void Disply(List<int> ar,FillitrDelegate Filter,string title)
+        {
+            Console.WriteLine($"====={title}=====)");
+            foreach (int item in ar)
+            {
+                if(IsEven(item))
+                {
+                    Console.WriteLine(item);
+
+
+                }
+
+
+
+            }
+
+
+
+
+        }
+      
+      
+     
+    }
+}
