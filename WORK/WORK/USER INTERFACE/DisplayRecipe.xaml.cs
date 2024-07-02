3681863f70f1867b7a7b7efb7c31d808ceb34d09
Nqobza25 <132018223+Nqobza25@users.noreply.github.com>
2024-07-02 17:59:14 +0200

    Add files via upload

---
+++ WORK/WORK/USER INTERFACE/DisplayRecipe.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace poe3.USER_INGREDIENT_WINDOWS
+{
+    /// <summary>
+    /// Interaction logic for DisplayRecipe.xaml
+    /// </summary>
+    public partial class DisplayRecipe : Window
+    {
+        public DisplayRecipe()
+        {
+            InitializeComponent();
+        }
+    }
+}
