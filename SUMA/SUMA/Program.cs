70c49bdd1dfd4f32abd571e0b0eac8cfe0d5fe9e
Nqobza25 <132018223+Nqobza25@users.noreply.github.com>
2024-07-02 18:05:18 +0200

    Add files via upload

---
+++ SUMA/SUMA/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUMA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1,value2,value3=3,result=0;
+
+            Console.WriteLine("Enter the first value");
+            value1 = Convert.ToInt32(Console.ReadLine());
+
+           Console.WriteLine("Enter the second value");
+            value2 = Convert.ToInt32(Console.ReadLine());
+
+            if(value1 == value2)
+            {
+               result = (value1 + value2) * value3;
+                Console.WriteLine("The sum of the values is:"+result);
+                SumCheck(value1, value2, result);
+            }
+            else if(value1 != value2) 
+            {
+                result= value1 + value2;
+                Console.WriteLine("The sum of values is:"+result);
+                SumCheck(value1, value2, result);
+            }
+
+           
+
+            
+            Console.ReadLine();
+            
+        }
+        public static bool SumCheck(int value1, int value2, int result)
+        {
+            if (value1 == 30 || value2 == 30 ||result == 30) { result = value1 + value2;
+                Console.WriteLine(true);
+            }
+               
+            else if (value1 != 30 && value2 != 30 && result != 30) {
+                Console.WriteLine(false); }
+            return true;
+        }
+    }
+}
---
+++ TestingStudentLibrary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using StudenLibray1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestingStudentLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student st = new Student("ST123","Micheal","Smith",21);
+            Student.AddStudent(new Student("ST123", "Serena", "Smith", 48));
+            Student.AddStudent(new Student("ST124", "Rodger", "Smith", 50));
+            Student.AddStudent(new Student("ST15", "Micheal", "Smith",30));
+            Student.AddStudent(new Student("ST126", "Jessica", "Jones", 11));
+            Student.AddStudent(new Student("ST127", "Micheal", "Specter",35));
+            Student.AddStudent(new Student("ST128", "Kevin", "De bruyne", 28));
+            Student.AddStudent(new Student("ST129", "Novak", "Djokovic", 29));
+            foreach (Student st in Student.AllStudents())
+            {
+                Console.WriteLine(st.ToString());
+            }
+            Console.Read();
+            Console.Beep();
+        }
+       
+    }
+}
